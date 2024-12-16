3686f0db260661a23cfe8ba501f41d6ebd575065
Henrique Rossin <pro.henriquerossin@gmail.com>
2024-12-15 22:15:34 -0300

    tentativa de deploy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 AOS.init();
+
+const dataDoEvento = new Date("Nov 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
+    console.log(diasAteOEvento);
+}, 1000);
