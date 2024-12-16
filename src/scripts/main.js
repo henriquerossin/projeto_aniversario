fbc86a211530b8243b454359e5311e76da77dd7a
Henrique Rossin <pro.henriquerossin@gmail.com>
2024-12-16 06:34:01 -0300

    ajuste no javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ AOS.init();
 const dataDoEvento = new Date("Dec 12, 2025 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
-const contaAsHoras = setInterval(function() {
+const contaAsHoras = setInterval(function () {
     const agora = new Date();
     const timeStampAtual = agora.getTime();
 
